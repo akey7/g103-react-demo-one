0c449efd9bb04153c27e3c3942776058b45b7667
Alicia Key <alicia.key@galvanize.com>
2018-11-20 10:47:03 -0700

    Initial state added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        { id: 0, item: 'Old AM radio.', reason: 'Hear crackles in the summer.' },
+        { id: 1, item: 'Shovel', reason: 'Because I hate telephone wires.' },
+        { id: 2, item: '2m/70cm dual band transceiver', reason: 'Because MORE RADIOS!'}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
