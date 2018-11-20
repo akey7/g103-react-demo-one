f6210a0696d3056203ba3506fbc58b24767d8ed8
Alicia Key <alicia.key@galvanize.com>
2018-11-20 13:26:04 -0700

    Setting state on controlled components good

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react'
 
 class AddItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      item: '',
+      reason: ''
+    }
+  }
+
   onSubmit = (e) => {
     e.preventDefault()
-    console.log(`I would submit now.`)
+    const { item, reason } = this.state
+    console.log(`Item: ${item} Reason: ${reason}`)
   }
-  
+
+  onReasonChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      ...this.state,
+      reason: e.target.value
+    })
+  }
+
+  onItemChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      ...this.state,
+      item: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>Add an item!</h3>
         <form onSubmit={this.onSubmit}>
           <div>
-            <label>Name: </label>
-            <input />
+            <label>Item: </label>
+            <input value={this.state.item} onChange={this.onItemChange} />
           </div>
           <div>
             <label>Reason: </label>
-            <input />
+            <input value={this.state.reason} onChange={this.onReasonChange} />
           </div>
           <input type='submit' />
         </form>
