e65308a847240aff120da7c6502c50b3114c81a8
Alicia Key <alicia.key@galvanize.com>
2018-11-20 12:34:02 -0700

    The item list now deletes

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -3,7 +3,7 @@ import Item from '../item/item'
 
 const ItemList = ({ items, onDeleteItem }) => (
   <ul>
-    { items.map((item, idx) => <Item key={idx} id={item.id} item={item} onDeleteItem={onDeleteItem} />)}
+    { items.map((item, idx) => <Item key={item.id} id={item.id} item={item} onDeleteItem={onDeleteItem} />)}
   </ul>
 )
 
